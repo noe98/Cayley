b18d7fbb136bb6be9ec11d7b897e89ec0472ed9c
pusztayj <pusztayj20@mail.wlu.edu>
2018-02-03 00:05:30 -0500

    Finished initial plans of CayleyTree class, by creating the nodes and links of the Cayley Tree object added comments to each method. The class currently generates the tree as well as two basic stats of number of nodes and number of nodes per generation.

--- CayleyTreeLinkGenerator.py
+++ CayleyTreeLinkGenerator.py
@@ -16,12 +16,15 @@ import xlwt
 import math
 
 class CayleyTree(object):
-    """Creates the Cayley Tree object."""
-
-    def __init__(self):
-        """Initializes an empty list reserved for the tuples representing 
-           each link, a dictionary for nodes and their states, and the
-           constants: alpha, gamma, and beta in the transition rate."""
+    """Creates the Cayley Tree object. The class needs integer values
+       for number of generations and links."""
+    
+    def __init__(self,generations,links):
+        """Initializes an empty list reserved for the tuples
+           representing each link, a dictionary for nodes and their states,
+           and the constants: alpha, gamma, and beta in the transition rate."""
+        self.generations = generations
+        self.links = links
         self.link_list = list()
         self.node_state_dict = dict()
         self.gamma = .1
@@ -42,6 +45,31 @@ class CayleyTree(object):
         """Changes the value of beta from the default value
            of 0.5. Must input a float between 0 and 1."""
         self.alpha = newAlpha
+
+    def nodeCalculator(self):
+        """Calculates the total number of nodes with a Cayley Tree and the
+           list of nodes per generation. Returns those two data types as a
+           tuple."""
+        list_of_nodes_by_generation = [1]
+        for x in range(1,self.generations+1):
+            nodes = (self.links * (self.links - 1)**(x - 1))
+            list_of_nodes_by_generation.append(nodes)
+        return (sum(list_of_nodes_by_generation),list_of_nodes_by_generation)
+
+    def linkCreator(self): 
+        """Generates the tuples that represents each link in the Cayley Tree
+           graph. It appends each tuple to the link_list list that is iniitiated
+           when the Cayley Tree object is created."""
+        self.nodeCalculator()
+        exclude = self.nodeCalculator()[1][-1]
+        for x in range(1,self.links + 1):
+            self.link_list.append((0,x))
+        nodes_done = self.links + 1
+        if nodes_done < self.nodeCalculator()[0]:
+            for x in range(1,self.nodeCalculator()[0] - exclude):
+                for y in range(1,self.links):
+                    self.link_list.append((x,nodes_done))
+                    nodes_done += 1
         
 
 gamma = .1
