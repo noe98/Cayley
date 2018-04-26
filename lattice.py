3282b8e314ed785ab2c881764ba65eb58b8a9dda
pusztayj <pusztayj20@mail.wlu.edu>
2018-04-26 19:39:24 -0400

    Added link creator

--- lattice.py
+++ lattice.py
@@ -1,37 +1,57 @@
 class Lattice(object):
 
     def __init__(self,length,width,height):
-        self.length = length
-        self.wideth = width
-        self.height = height
+        self.x = length
+        self.y = width
+        self.z = height
 
     def __eq__(self,other):
         if self is other:
             return True
         if type(self) != type(other):
             return False
-        elif self.length == other.length and self.width == other.width and \
-             self.height == other.height:
+        elif self.x == other.x and self.y == other.y and \
+             self.z == other.z:
             return True
         else:
             return False
 
     def latticeProtect(self):
-        if self.width < 0 or self.length < 0 or self.height < 0:
+        if self.y < 0 or self.x < 0 or self.z < 0:
             raise ValueError("Inappropriate entries lattice cannot \
                              exist")
     def nodeNumber(self):
-        """Returns the total number of nodes in the Cayley tree. """
-        if self.height < 0:
-            return self.length*self.width
-        return self.height*self.width*self.length
+        """Returns the total number of nodes in the Lattice."""
+        if self.z < 0:
+            return self.x*self.y
+        return self.z*self.y*self.x
 
     def linkCreator(self):
         link_d = dict()
-        for n in list(range(self.nodeNumber())):
-            if n%self.length != 0:
-                link_d[n] = [n+1]
-##            if True:
-##                link_d[n]
-            
-            
+        #in x-direction
+        for count in range(self.x*self.y):
+            if count % self.x == 0:
+                link_d[count] = [count+1]
+            elif count % self.x == self.x-1:
+                link_d[count] = [count-1]
+            elif count-1<0:
+                link_d[count] = [count+1]
+            else:
+                link_d[count+1] = [count-1]
+                link_d[count] = link_d.get(count+1,list()) + [count+1]
+                
+        #in y-direction
+        for count in range(self.x*self.y):
+            if count >= (self.x*self.y)-self.x:
+                link_d[count] = link_d.get(count,list())+ [count-self.x]
+                link_d[count-self.x] = link_d.get(count-self.x,list()) + [count]
+            else:
+                if count+self.x >= (self.x*self.y)-self.x:
+                    pass
+                else:
+                    link_d[count+self.x] = link_d.get(count+self.x,list()) + [count]
+                    link_d[count] = link_d.get(count,list()) + [count+self.x]
+        #in-z-dirction
+        
+        return link_d
+    
