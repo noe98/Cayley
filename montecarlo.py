2c1e549da3cc15cad0a8f7ac52a3861f713fae4e
Billy-The-Squid <39772580+Billy-The-Squid@users.noreply.github.com>
2018-06-01 14:16:37 -0400

    Fix TL Issue

--- montecarlo.py
+++ montecarlo.py
@@ -25,8 +25,8 @@ class MonteCarlo(object):
                  r1 = 0.3, r2 = 0.5):
         """Runs the Monte Carlo simulation the desired number of times."""
         self.network = network
-        self.state_d = dict()
-        self.list_cache = None
+        self.state_d = dict() ## Is dict. w/ node# key
+        self.list_cache = None ## Is array w/ timestep first index, node# second
         self.alpha = alpha
         self.beta = beta
         self.gamma = gamma
@@ -198,23 +198,28 @@ class MonteCarlo(object):
     def simulateTL(self,timestep): #Only works for first timestep
         """Simulates the Monte Carlo simulation on the Cayley Tree for one
            time step and stores that data."""
-        time_steps = range(len(self.state_d)) 
+        print("Timestep: " + str(timestep))
+        time_steps = range(len(self.state_d))
+        f=1#sho
+        for x in range(1,self.network.generations+1):#sho
+            f= f+ self.network.links*(self.network.links-1)**(x-1)#sho
+        print("f: " +str(f))
         if self.list_cache == None:
             list_cache = list()
             list_cache.append(self.state_d)
         else:
             list_cache = self.list_cache
         cache = dict()
-        no_nodes = (self.network.links*(self.network.links-1)**(self.network.generations-1))
+        #no_nodes = (self.network.links*(self.network.links-1)**(self.network.generations-1))
         if timestep == 0:
             dens = 0 #density function
         else:
-            dens = self.getOnes(timestep)/no_nodes ### make sure this calls correct timestep
+            dens = self.getOnes(timestep)/f ### make sure this calls correct timestep
+        print("dens: " +str(dens))
         for x in self.network:
-            summ = self.nearestNeighborSum(x,list_cache[-1])
-            #print("summ: ", summ)
             probability = self.gamma*list_cache[-1][x] + \
                                     (1 - list_cache[-1][x])*(1-dens)*self.mu
+            print("probability: " +str(probability))
             if random.uniform(0, 1) <= probability and list_cache[-1][x] == 0:
                 cache[x] = 1
             elif random.uniform(0, 1) <= probability and \
@@ -222,7 +227,7 @@ class MonteCarlo(object):
                 cache[x] = 0 
             else:
                 cache[x] = list_cache[-1][x]
-        #print("cache: ",cache)
+        print("cache: ",cache)
         list_cache.append(cache)
         self.list_cache = list_cache
         return self.list_cache
--- volume.py
+++ volume.py
@@ -15,21 +15,12 @@ import xlsxwriter as xl
 import time
 from math import sqrt
 
-total_nodes = [[1,None,None,None,None], #total_nodes[gens][links]
-               [None,2,3,4,5],
-               [None,None,5,10,17],
-               [None,None,7,22,53],
-               [None,None,9,46,161],
-               [None,None,11,94,485]]
-
 alpha_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 beta_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 gamma_list = [0, 0.05, 0.1, 0.15, 0.2]
 mu_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 r1_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 r2_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-
-timesteps = 25
 
 def simulate(method, generations, links, alpha, beta, gamma, mu, r1, r2, trials):
     """The important one"""
@@ -44,24 +35,21 @@ def simulate(method, generations, links, alpha, beta, gamma, mu, r1, r2, trials)
     if method == 'NN':
         name = ("NN%dGen_%dLin_%sα_%sβ_%sγ.xlsx" % (generations, links, a_tag, b_tag, g_tag))
     elif method == 'TL':
-        name = ("TL%dGen_%dLin_%sμ_%sγ.xlsx" % (generations, links, m_tag, g_tag))
+        name = ("TL%dGen_%dLin_%sαμ_%sγ.xlsx" % (generations, links, m_tag, g_tag))
     elif method == 'EI':
         name = ("EI%dGen_%dLin_%sr1_%sr2_%sγ.xlsx" % (generations, links, r1_tag, r2_tag, g_tag))
     else: raise ValueError("Method not recognized")
     workbook = xl.Workbook(name)
     density_list = {}
-    other_density_list = {} #key is trial#, index is timestep, gives density
-    worksheet4 = workbook.add_worksheet("Over Time")
     worksheet3 = workbook.add_worksheet("Overall")
     
     for j in range(generations+1):
         density_list[j] = []    #First index is generation, 
                                 #second is maybe trial number?    
     for i in range(trials):
-        other_density_list[i] = [0]*(timesteps+2)
         monte.emptyDictionary()
         monte.list_cache = None
-        for j in range(timesteps+1):
+        for j in range(len(monte.network)):
             if method == 'NN':
                 monte.simulateNN()
             elif method == 'EI':
@@ -81,25 +69,14 @@ def simulate(method, generations, links, alpha, beta, gamma, mu, r1, r2, trials)
 
         worksheet2 = workbook.add_worksheet("Density trial %d" % (i))
         worksheet2.write(0,0,"Timestep")
-        worksheet2.write(monte.network.generations+2,0,"Total")
-        worksheet2.write(monte.network.generations+3,0,"Density")        
         for x in range(monte.network.generations+1):
             worksheet2.write(x+1,0,"Gen. "+str(x))
-        for y in range(timesteps+1):
+        for y in range(len(monte.state_d)):
             worksheet2.write(0,y+1,str(y))
         for y in range(len(monte.list_cache)):
             for x in range(monte.network.generations+1):
                 worksheet2.write(x+1,y+1,monte.densityCalculator(x,monte.list_cache[y]))
-        for y in range(len(monte.list_cache)):
-            sum_t = 0
-            for x in range(len(monte.network)):
-                sum_t += monte.list_cache[y][x]
-            dens_t = sum_t/total_nodes[monte.network.generations][monte.network.links]
-            worksheet2.write(monte.network.generations+2,y+1,sum_t)
-            worksheet2.write(monte.network.generations+3,y+1,dens_t)
-            ## worksheet4.write(i+2,y+1,dens_t)
-            other_density_list[i][y] = dens_t
-        
+
         for k in range(generations+1):
             density_list[k].append(monte.densityCalculator(k,monte.list_cache[len(monte.list_cache)-1]))
 
@@ -143,18 +120,6 @@ def simulate(method, generations, links, alpha, beta, gamma, mu, r1, r2, trials)
     worksheet3.write(generations+2,trials+2,str(tot_dens))
     SD_all = sqrt((tot_dens)*(1-tot_dens)/(trials*sum(node_list)))
     worksheet3.write(generations+2,trials+3,str(SD_all))
-
-    worksheet4.write(0,0,"Timestep")
-    worksheet4.write(1,0,"Average")
-    #for t in range(trials):
-        #worksheet4.write(t+2,0,"Trial "+str(t))
-    for k in range(timesteps+1):
-        t_sum = 0
-        worksheet4.write(0,k+1,k)
-        for t in range(trials):
-            t_sum += other_density_list[t][k]
-        t_av = t_sum/trials
-        worksheet4.write(1,k+1,t_av)
     
     workbook.close
         
@@ -175,7 +140,6 @@ def main():
         gamma = float(input("Value for gamma: "))
         alpha = beta = r1 = r2 = 0
     elif method == 'EI':
-        print("R1 should be less than R2 for electrostatic models.")
         r1 = float(input("R1 value: "))
         r2 = float(input("R2 value: "))
         gamma = float(input("Value for gamma: "))
@@ -233,8 +197,6 @@ def full(method, generations, links, trials):
                 simulate('TL', generations, links, alpha, beta, g, m, r1, r2, trials)
     print("--- runtime is %s seconds ---" % (time.time() - start_time))
     
-def timestep(number_of_timesteps):
-    timesteps = number_of_timesteps
 
 if __name__ == "__main__":
     main()
