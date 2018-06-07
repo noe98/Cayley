bf0ce360ced55038b617ac4d668fb3ae92c3c37e
Billy-The-Squid <39772580+Billy-The-Squid@users.noreply.github.com>
2018-06-07 12:36:28 -0400

    Less data usage, hopefully faster

--- volume.py
+++ volume.py
@@ -2,12 +2,9 @@
 Adapted from cayleymain module by Justin Pusztay et al.
 Project: Nanoparticle deposition
 
-Hopefully this will run more than one simulation at a time, so that I can
-gather data quickly. We'll see what happens.
+Runs Cayley tree simulations in bulk according to the methods laid out in
+montecarlo.py
 """
-
-# N.B. Because I'm lazy, don't run this with parameters that go beyond
-# two decimal places. Or if you want to, change how the file name is saved. 
 
 import Cayley as cy
 import Cayley.graphics as cg
@@ -31,11 +28,14 @@ r2_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 
 timesteps = 50
 
+## # <-- indicates adjusted generations (account for last gen fluctuations)
+
 def simulate(method, generations, links, alpha, beta, gamma, mu, r1, r2, trials):
     """The important one"""
     generations = generations + 1 ## #
     network = cy.CayleyTree(generations, links)
     monte = cy.MonteCarlo(network, alpha, beta, gamma, mu, r1, r2)
+
     a_tag = "%.2f" % alpha
     b_tag = "%.2f" % beta
     g_tag = "%.2f" % gamma
@@ -49,17 +49,21 @@ def simulate(method, generations, links, alpha, beta, gamma, mu, r1, r2, trials)
     elif method == 'EI':
         name = ("EI%dGen_%dLin_%sr1_%sr2_%sγ.xlsx" % (generations-1, links, r1_tag, r2_tag, g_tag))
     else: raise ValueError("Method not recognized")
-    workbook = xl.Workbook(name)
-#    density_list = {}
-    other_density_list = {} #key is trial#, index is timestep, gives density
-    worksheet4 = workbook.add_worksheet("Over Time")
-#    worksheet3 = workbook.add_worksheet("Overall")
     
-#    for j in range(generations+1):
-#        density_list[j] = []    #First index is generation, 
-                                #second is maybe trial number?    
+    workbook = xl.Workbook(name)
+    density_list = {} #[trial][timestep] stores overall densities
+    state_collect = {} #[trial] stores final state dictionaries
+    dens_collect = {} #[trial][generation] stores generational densities
+    overtime = workbook.add_worksheet("Over Time")
+    overall = workbook.add_worksheet("Overall")
+
+    for k in range(trials):
+        density_list[k] = [0]*(timesteps+2)
+
+    for k in range(trials):
+        dens_collect[k] = [0]*(generations)
+        
     for i in range(trials):
-        other_density_list[i] = [0]*(timesteps+2)
         monte.sim_data = []
         monte.emptyDictionary()
         for j in range(timesteps+1):
@@ -69,94 +73,98 @@ def simulate(method, generations, links, alpha, beta, gamma, mu, r1, r2, trials)
                 monte.simulateEI()
             elif method == 'TL':
                 monte.simulateTL(j)
-        
-#        worksheet = workbook.add_worksheet("Data trial %d" % (i))
-#        worksheet.write(0,0,"Timestep")
-#        for x in range(total_nodes[generations-1][links]):## #
-#            worksheet.write(x+1,0,"Node "+str(x))
-#        for y in range(len(monte.sim_data)):
-#            worksheet.write(0,y+1,str(y))
-#        for y in range(len(monte.sim_data)):
-#            for x in range(total_nodes[generations-1][links]):
-#                worksheet.write(x+1,y+1,monte.sim_data[y][x])
 
-        worksheet2 = workbook.add_worksheet("Density trial %d" % (i))
-        worksheet2.write(0,0,"Timestep")
-        worksheet2.write(monte.network.generations+1,0,"Total") ## #
-        worksheet2.write(monte.network.generations+2,0,"Density") ## #       
-        for x in range(monte.network.generations): ## #
-            worksheet2.write(x+1,0,"Gen. "+str(x))
-        for y in range(timesteps+1):
-            worksheet2.write(0,y+1,str(y))
+        ### FOR RECORDING DATA ###
+        state_collect[i] = monte.sim_data[(len(monte.sim_data)-1)]
+                
         for y in range(len(monte.sim_data)):
-            for x in range(monte.network.generations+1): ## # double-check line below
-                worksheet2.write(x+1,y+1,monte.density(x,monte.sim_data[y]))
-        for y in range(len(monte.sim_data)):
-            sum_t = 0
-            for x in range(total_nodes[generations-1][links]): ## #
+            sum_t = 0 # Sum of relevant nodes at one timestep
+            for x in range(total_nodes[generations-1][links]): ## # gives adjusted, can't use len(monte.network)
                 sum_t += monte.sim_data[y][x]
-            dens_t = sum_t/total_nodes[generations-1][links] ## #
-            worksheet2.write(monte.network.generations+1,y+1,sum_t) ## #
-            worksheet2.write(monte.network.generations+2,y+1,dens_t) ## #
-            ## worksheet4.write(i+2,y+1,dens_t)
-            other_density_list[i][y] = dens_t
+            dens_t = sum_t/total_nodes[generations-1][links] ## # Density at one timestep
+            density_list[i][y] = dens_t
+
+        if trials <= 10: # Trial-by-trial is best for small sets
+            worksheet = workbook.add_worksheet("Data trial %d" % (i+1))
+            worksheet.write(0,0,"Timestep")
+            for x in range(total_nodes[generations-1][links]):## #
+                worksheet.write(x+1,0,"Node "+str(x))
+            for y in range(len(monte.sim_data)):
+                worksheet.write(0,y+1,str(y))
+            for y in range(len(monte.sim_data)):
+                for x in range(total_nodes[generations-1][links]):
+                    worksheet.write(x+1,y+1,monte.sim_data[y][x])
+
+            worksheet2 = workbook.add_worksheet("Density trial %d" % (i+1))
+            worksheet2.write(0,0,"Timestep")
+            worksheet2.write(monte.network.generations+1,0,"Density") ## #       
+            for x in range(monte.network.generations): ## #
+                worksheet2.write(x+1,0,"Gen. "+str(x))
+            for y in range(timesteps+1):
+                worksheet2.write(0,y+1,str(y))
+            for y in range(len(monte.sim_data)):
+                for x in range(monte.network.generations+1): ## # double-check line below
+                    worksheet2.write(x+1,y+1,monte.density(x,monte.sim_data[y]))
+                worksheet2.write(monte.network.generations+1,y+1,density_list[i][y]) ## #
         
-#        for k in range(generations+1):
-#            density_list[k].append(monte.density(k,monte.sim_data[len(monte.sim_data)-1]))
+    ### FOR RECORDING OVERALL DATA ###
+    overall.write(0,0,"Generation") # For steady-state analysis
+    for x in range(generations):
+        overall.write(x+1,0,"Gen. "+str(x))
+    overall.write(0,1,"Average")
+    overall.write(0,2,"Std Dev")
 
-### ADD BACK IN IF NEEDED FOR STEADY-STATE
-##    worksheet3 = workbook.add_worksheet("Overall")
-#    worksheet3.write(0,0,"Trial")
-#    for x in range(generations+1):
-#        worksheet3.write(x+1,0,"Gen. "+str(x))
-#    for y in range(trials):
-#        worksheet3.write(0,y+1,"No. "+str(y))
-#    worksheet3.write(generations+2,0,"Total")
-#    worksheet3.write(0,trials+1,"Average")
-#    worksheet3.write(0,trials+2,"Density")
-#    worksheet3.write(0,trials+3,"Std Dev")
-#    for x in range(generations+1):
-#        for y in range(trials):
-#            worksheet3.write(x+1,y+1,str(density_list[x][y]))
-#    node_list = []      # Note: it may be a nice idea to rearrange
-#    for x in range(generations+1):      #node_list, no_nodes, etc. so that
-#        sum_gen = sum(density_list[x])  #they are calculated early on.
-#        av = sum_gen/(trials)
-#        if x == 0:
-#            no_nodes = 1
-#        else:
-#            no_nodes = (monte.network.links*(monte.network.links-1)**(x-1))
-#        SD = sqrt((av/no_nodes)*(1-av/no_nodes)/(trials*no_nodes))
-#        worksheet3.write(x+1,trials+1,str(av))
-#        worksheet3.write(x+1,trials+2,str(av/no_nodes))
-#        worksheet3.write(x+1,trials+3,str(SD))
-#        node_list.append(no_nodes)
-#    all_list = []
-#    for y in range(trials):
-#        trial_list = []
-#        for x in range(generations+1):
-#            trial_list.append(density_list[x][y])
-#        trial_sum = sum(trial_list)
-#        all_list.append(trial_sum)
-#        tot_av = sum(all_list)/trials
-#        tot_dens = tot_av/sum(node_list)
-#        worksheet3.write(generations+2,y+1,str(trial_sum))
-#    worksheet3.write(generations+2,trials+1,str(tot_av))
-#    worksheet3.write(generations+2,trials+2,str(tot_dens))
-#    SD_all = sqrt((tot_dens)*(1-tot_dens)/(trials*sum(node_list)))
-#    worksheet3.write(generations+2,trials+3,str(SD_all))
+    for x in range(generations):
+        if x == 0:
+            for y in range(trials):
+                dens_collect[y][x] = monte.density(x,state_collect[y])
+        else:
+            for y in range(trials):
+                dens_collect[y][x] = monte.density(x,state_collect[y])/ \
+                                     ((links)*(links-1)**(x-1))
+    
+    if trials <= 10: # Densities per gen for individual trials
+        for y in range(trials):
+            overall.write(0,y+4,"Trial "+str(y+1))
+            overall.write(generations+1,y+4,density_list[y][timesteps])
+        for x in range(generations):
+            for y in range(trials):
+                overall.write(x+1,y+4,dens_collect[y][x])
 
-    worksheet4.write(0,0,"Timestep")
-    worksheet4.write(1,0,"Average")
-    #for t in range(trials):
-        #worksheet4.write(t+2,0,"Trial "+str(t))
+    for x in range(generations): # Average density per generation
+        list_gen = [] # Temporary variable
+        for y in range(trials):
+            list_gen.append(dens_collect[y][x])
+        av = sum(list_gen)/(trials)
+        SD = sqrt((av)*(1-av)/total_nodes[generations-1][links])
+        overall.write(x+1,1,str(av))
+        overall.write(x+1,2,str(SD))
+
+    overall.write(generations+1,0,"Total")
+    tot_sum = 0
+    for c in range(trials):
+        tot_sum += density_list[c][timesteps]
+    tot_av = tot_sum/trials
+    SD_all = sqrt((tot_av)*(1-tot_av)/(trials*total_nodes[generations-1][links]))
+    overall.write(generations+1,1,tot_av)
+    overall.write(generations+1,2,SD_all)
+
+    # Average density over time
+    overtime.write(0,0,"Timestep")
+    overtime.write(1,0,"Average")
+    if trials <= 10:
+        for t in range(trials):
+            overtime.write(t+3,0,"Trial "+str(t+1))
+            for k in range(timesteps+1):
+                overtime.write(t+3,k+1,density_list[t][k])
+    else: overtime.write(3,0,"Trials: "+str(trials))
     for k in range(timesteps+1):
         t_sum = 0
-        worksheet4.write(0,k+1,k)
+        overtime.write(0,k+1,k)
         for t in range(trials):
-            t_sum += other_density_list[t][k]
+            t_sum += density_list[t][k]
         t_av = t_sum/trials
-        worksheet4.write(1,k+1,t_av)
+        overtime.write(1,k+1,t_av)
     
     workbook.close
         
@@ -263,6 +271,5 @@ if __name__ == "__main__":
 
     
 ## Things to look at/fix:
-## Make sure all simulate() calls have correct args
-## Finish range functions
-## Find issue with EI
+## Clean up formatting
+## Make this able to call the lattice class
